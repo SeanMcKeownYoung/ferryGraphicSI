bbde676817100f85721ffeab4eeaf2255cccecbd
Sean McKeown-Young <117399937+SeanMcKeownYoung@users.noreply.github.com>
2023-05-04 10:36:03 -0400

    Commit Final 01

--- js/index.js
+++ js/index.js
@@ -54,3 +54,48 @@ function myFunction() {
       x.style.display = "none";
     }
   }
+  const faders = document.querySelectorAll
+  ('.fade-in');
+
+const appearOptions = {
+    threshold: 1
+};
+
+  const appearOnScroll = new IntersectionObserver
+  (function(
+    entries, 
+    appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, 
+    appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
+      
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+      
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
+          }
+        }
+      }
+      
+      window.addEventListener("scroll", reveal);
+      
